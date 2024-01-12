1d8cc01818fbabbbce97c95f5a53fea29c081be4
Valeriya K <saleria-v@mail.ru>
2024-01-13 02:27:51 +0300

    feat auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,19 @@
-module.exports = (req, res, next) => {
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { verify } from 'jsonwebtoken';
+import { UNAUTHORIZED_ERROR } from '../responses/responses';
 
+export default (req, res, next) => {
+  const { autorization } = req.headers;
+  if (!autorization || !autorization.startsWith('Bearer ')) {
+    return res.status(UNAUTHORIZED_ERROR).send({ message: 'Необходима авторизация' });
+  }
+  const token = autorization.replace('Bearer ', '');
+  let payload;
+  try {
+    payload = verify(token, 'some-secret-key');
+  } catch (err) {
+    return res.status(UNAUTHORIZED_ERROR).send({ message: 'Необходима авторизация' });
+  }
+  req.user = payload;
+  next();
 };
