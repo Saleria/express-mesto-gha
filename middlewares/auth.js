ae71b00ca5d128dbab1fa10a9ef57fbd643d9996
Valeriya K <saleria-v@mail.ru>
2024-01-15 23:48:37 +0300

    fix auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,20 @@
 const jwt = require('jsonwebtoken');
-const { UNAUTHORIZED_ERROR } = require('../responses/responses');
+const Unauthorized = require('../errors/unauthorized-err');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(UNAUTHORIZED_ERROR).send({ message: 'Необходима авторизация' });
+    next(new Unauthorized('Необходима авторизация'));
+    return;
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return res.status(UNAUTHORIZED_ERROR).send({ message: 'Необходима авторизация' });
+    next(new Unauthorized('Необходима авторизация'));
+    return;
   }
   req.user = payload;
-  return next();
+  next();
 };
