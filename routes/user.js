1ddd0ce47b292234dabf301de9ff5a044bb1f3fc
Valeriya K <saleria-v@mail.ru>
2024-01-15 23:37:29 +0300

    fix routes

--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,9 @@ const {
 const { getUserByIdValidation, updateUserValidation, updateAvatarValidation } = require('../middlewares/validator');
 
 router.get('/', getUser);
+router.get('/me', getCurrentUser);
 router.get('/:userId', getUserByIdValidation, getUserById);
 router.patch('/me', updateUserValidation, updateUser);
 router.patch('/me/avatar', updateAvatarValidation, updateAvatar);
-router.get('/me', getCurrentUser);
 
 module.exports = router;
